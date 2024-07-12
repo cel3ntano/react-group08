8b4304d719beeab7333e5919ed0c2bf6c975069e
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-07-12 22:28:24 +0200

    3 tasks

---
+++ src/tasks/task2.js
@@ -0,0 +1,31 @@
+// const string = 'string';
+
+// const firstToUpper = string => {
+//   return string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
+// };
+
+// const string = 'string';
+
+// const firstToUpper = string => {
+//   return string.replace(string[0], string[0].toUpperCase());
+// };
+
+// console.log(firstToUpper(string)); // 'String'
+
+// const string = 'string';
+
+// const firstToUpper = string => {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// };
+
+// console.log(firstToUpper(string)); // 'String'
+
+
+// const string = 'string';
+
+// const firstToUpper = string => {
+//   const [first, ...rest] = string;
+//   return first.toUpperCase() + rest.join('');
+// };
+
+// console.log(firstToUpper(string)); // 'String'
