bed1aeb4af04d99a226ef406d71bb3e7b2916ae2
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-08-11 22:42:04 +0300

    tasks-16

---
+++ tasks80/TASKS/15-replace-part-string/start.js
@@ -0,0 +1,12 @@
+/** ЗАДАЧА 15 - Замена части строки
+ *
+ * 1. Создайте переменную и присвойте ей строку "Good Morning"
+ *
+ * 2. Замените "Morning" на "Evening" и присвойте результат новой переменной
+ *
+ * 3. Выведите значение второй переменной в консоль
+ */
+
+const peremennaya = "Good Morning";
+const newPer = peremennaya.replace("Morning", "Evening");
+console.log(newPer);
